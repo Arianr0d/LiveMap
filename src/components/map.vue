fc24bb2fddc66c70de9aea63b3d11dc2eb2830fa
Tatyana Komolyh <tatyana.comolyh@yandex.ru>
2021-09-06 14:24:37 +0300

    start

---
+++ src/components/map.vue
@@ -0,0 +1,13 @@
+<template>
+  <l-map style="height: 300px" :zoom="zoom" :center="center">
+    <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
+    <l-marker :lat-lng="markerLatLng"></l-marker>
+  </l-map>
+</template>
+
+<script>
+
+export default {
+   name: 'map'
+}
+</script>
