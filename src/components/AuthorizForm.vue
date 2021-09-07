25844a653b2572327373dbbf1d67e1d240e791f9
Tatyana Komolyh <tatyana.comolyh@yandex.ru>
2021-09-07 14:58:37 +0300

    signin

---
+++ src/components/AuthorizForm.vue
@@ -0,0 +1,46 @@
+<template>
+   <div>
+      <form action="POST">
+         <p class="title">{{ titleName.titleName }}</p>
+         <div class="group">
+            <input type="login" class="inp" placeholder="Логин"/>
+         </div>   
+         <div v-if="signIn" class="group">
+            <input type="text" class="inp" placeholder="Почта"/>
+         </div>
+         <div class="group">
+            <input type="password" class="inp" placeholder="Пароль"/>
+         </div>
+         <div v-if="signIn" class="group">
+            <input type="password" class="inp" placeholder="Подтвердить пароль"/>
+         </div>
+         <button type="button" class="button" id="btn">{{ titleName.buttonName }}</button>
+         <div class="bottom_group" v-if="!signIn">
+            <a class="link" href="">Забыли пароль?</a>
+            <a class="link" href="">Зарегистрироваться</a>
+         </div>
+      </form>      
+   </div>
+</template>
+
+<script>
+
+export default {
+   name: 'AuthorizForm',
+   props: {
+      titleName: { type: Object },
+      signIn: {
+         type: Boolean,
+         default: false
+      }
+   }
+}
+
+</script>
+
+<style scoped>
+   * {
+      margin: 0;
+      padding: 0;
+   }
+</style>
