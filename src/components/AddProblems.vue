be016f3af69b20524d867a7344b720dae464ba63
Tatyana Komolyh <tatyana.comolyh@yandex.ru>
2021-09-07 16:35:01 +0300

    addproblems

---
+++ src/components/AddProblems.vue
@@ -0,0 +1,30 @@
+<template>
+   <div class="form">
+      <div>
+         <img src="../assets/img/titleicon.svg">
+         <p>Добавление проблемы</p>
+      </div>
+      <div class="group">
+         <input type="" class="inp" placeholder="адрес"/>
+         <msg src="../assets/img/Flag.svg">
+      </div>
+      <!-- выпадающий список -->
+      <input class="inp" placeholder="комментарий"/>
+      <div class="button_group">
+         <button type="button">Отменить</button>
+         <button type="button">Добавить</button>
+      </div>
+   </div>
+</template>
+
+<script>
+
+export default {
+   name: 'AddProblems'
+}
+
+</script>
+
+<style scoped>
+
+</style>
